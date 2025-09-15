6a5ad340ffbcbd0b617cc2812bdad657db5ee152
Senthil <senthilkumari24p@gmail.com>
2025-09-15 14:43:57 -0400

    feat: add childeren routes and lazy loading

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
 import ReactDOM from "react-dom/client";
+import { lazy, Suspense } from "react";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
 import Footer from "./src/components/Footer";
-
-
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import ErrorPage from "./src/components/Error";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+const Grocery = lazy(() => import("./src/components/Grocery"));
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Body />
-    
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<div>Loading Groceries</div>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <ErrorPage />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -9,9 +12,19 @@ const Header = () => {
       </div>
       <div className="list-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li>{onlineStatus ? "🟢" : "🔴"}</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
           <li>Cart</li>
           <li>
             {" "}
